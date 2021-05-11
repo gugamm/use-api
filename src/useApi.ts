9c1f6b5de97392c2cbe98a5584a3627e1530b9c5
Gustavo Martins <gugamm@live.com>
2021-05-11 15:43:44 +0200

    only setState on mounter component

--- src/useApi.ts
+++ src/useApi.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useRef, useEffect } from 'react'
 import { Fetcher, OperationState, OperationTrigger, InitialOperationState, LoadingOperationState, ErrorOperationState, SuccessOperationState } from './types'
 import { ExtractPromiseType } from './utilTypes'
 
@@ -37,15 +37,30 @@ const successState = <TData>(data: TData): SuccessOperationState<TData> => {
 export type UseApiResult<TArgs extends Array<any>, TData> = [OperationState<TData>, OperationTrigger<TArgs, TData>]
 export const useApi = <TFetcher extends Fetcher<any>>(fetcher: TFetcher): UseApiResult<Parameters<TFetcher>, ExtractPromiseType<ReturnType<TFetcher>>> => {
   const [operationState, setOperationState] = useState<OperationState<ExtractPromiseType<ReturnType<TFetcher>>>>(INITIAL_OPERATION_STATE)
+  const isMountedRef = useRef(true)
+
+  useEffect(() => {
+    isMountedRef.current = true
+    return () => {
+      isMountedRef.current = false
+    }
+  }, [])
+
   const operationTrigger = useCallback(async (...args: Parameters<TFetcher>) => {
-    setOperationState(LOADING_STATE)
+    if (isMountedRef.current) {
+      setOperationState(LOADING_STATE)
+    }
 
     try {
       const data = await fetcher(...args)
-      setOperationState(successState(data))
+      if (isMountedRef.current) {
+        setOperationState(successState(data))
+      }
       return successState(data)
     } catch (err) {
-      setOperationState(errorState(err))
+      if (isMountedRef.current) {
+        setOperationState(errorState(err))
+      }
       return errorState(err)
     }
   }, [fetcher])
